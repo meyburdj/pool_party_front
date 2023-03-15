bb4f43a64b2457a0307ecec543205ee1080ac716
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-03-14 18:07:18 -0700

    adding new messages components

--- src/Messages.js
+++ src/Messages.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import userContext from "./UserContext";
+import PoolPartyApi from './api';
+import React, { useState, useContext, useEffect } from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+/**
+ * Messages: Parent component for conversations and messages
+ * 
+ * Props: User
+ * State: messages
+ * 
+ * Messages -> contacts
+ * Messages -> [messageCard, messageCard, ...] 
+ */
 function Messages() {
+  const { user } = useContext(userContext);
+  const [messages, setMessages] = useState({
+    data: null,
+    isLoading: true,
+  });
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    textAlign: 'center',
+    borderColor: 'red',
+    color: theme.palette.text.secondary
+  }));
+
   return (
-    <div>Messages</div>
-  )
+    <Box sx={{ flexGrow: 1, borderColor: 'primary.main' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4} >
+
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+          <Item >xs=8</Item>
+        </Grid>
+
+        <Grid item xs={12} sm={8}>
+          {messages.map(x => (
+            x.from === user.name ?
+              <Box display="flex" justifyContent="flex-end">
+                <Grid sm={10}>
+                  <Item >xs=8</Item>
+                </Grid>
+              </Box> :
+              <Box display="flex" justifyContent="flex-end">
+                <Grid sm={10}>
+                  <Item >xs=8</Item>
+                </Grid>
+              </Box>
+          ))}
+
+        </Grid>
+
+      </Grid>
+    </Box>
+  );
 }
 
-export default Messages
+
+
+export default Messages;
+
+
+
