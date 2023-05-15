f9c43e163e1546e9e041235c00674dbf39a1facf
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-05-15 16:22:44 -0700

    changed backend url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
-const BASE_URL = "https://poolpartyback-production.up.railway.app";
+const BASE_URL = "https://poolpartyback-production.up.railway.app/api";
 
 /** API Class.
  *
